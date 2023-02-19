70d26c87d580f922e665a761295bd856087166bb
costaCarlos5 <48063279+costaCarlos5@users.noreply.github.com>
2023-02-19 14:01:02 +0000

    new version
    test

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 // This is an example of a simple JavaScript function to toggle the visibility of a menu
-
+/* 
 function toggleMenu() {
     var menu = document.querySelector("nav ul");
     menu.classList.toggle("show");
@@ -7,3 +7,27 @@ function toggleMenu() {
   
   document.querySelector(".menu-toggle").addEventListener("click", toggleMenu);
   
+ */
+
+  // Exemplo de código JavaScript
+var welcomeMessage = "Bem-vindo ao meu blog! ";
+
+function displayWelcomeMessage() {
+  var heading = document.querySelector("h2");
+  heading.textContent = welcomeMessage;
+}
+
+displayWelcomeMessage();
+
+// Adiciona evento ao botão de saudação
+var btnGreet = document.getElementById("btn-greet");
+btnGreet.addEventListener("click", greetUser);
+
+// Função de saudação do usuário
+function greetUser() {
+  var inputName = document.getElementById("name").value;
+  var greeting = "Olá, " + inputName + "! Bem-vindo ao meu blog!";
+  var heading = document.querySelector("h2");
+  heading.textContent = greeting;
+}
+
